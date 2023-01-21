b8d0a52479b999fc83d9da7e359f28f7345af06c
aliyurekli32 <adamjesperblack@gmail.com>
2023-01-21 05:27:01 +0300

    add ProductAdmin to admin

--- products/admin.py
+++ products/admin.py
@@ -3,8 +3,14 @@ from django.contrib import admin
 # Register your models here.
 from .models import Product
 
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "create_date", "is_in_stock", "update_date")
+    list_editable = ( "is_in_stock", )
+    list_display_links = ("name", )
+    list_filter = ("is_in_stock", "create_date")
+admin.site.register(Product,ProductAdmin)
 
 admin.site.site_title = "Clarusway Title"
 admin.site.site_header = "Clarusway Admin Portal"  
 admin.site.index_title = "Welcome to Clarusway Admin Portal"
+
