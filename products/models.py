eac5fa55c94493137d3067a763d8ffd19fe33e16
aliyurekli32 <adamjesperblack@gmail.com>
2023-01-21 10:03:18 +0300

    adding Review Model

--- products/models.py
+++ products/models.py
@@ -17,3 +17,21 @@ class Product(models.Model):
     
     def __str__(self):
         return self.name
+    
+    def how_many_reviews(self):
+        count = self.reviews.count()
+        return count
+
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
+    review = models.TextField()
+    is_released = models.BooleanField(default=True)
+    created_date = models.DateField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name = 'Review'
+        verbose_name_plural = 'Reviews'
+
+    def __str__(self):
+        return f"{self.product.name} - {self.review}"
